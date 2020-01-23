9bcb76cba4cd2925a4f923060b14e470157dd3b4
mhowe <matthew.howe@kineticdata.com>
2020-01-23 08:17:08 -0600

    Updated handlers to improve performance of disable caching logic
    The existing structure caused collisions when the handler was used in a loop and disabled caching was enabled.  Additional changes made to support better caching strategy.

--- handlers/remedy_generic_update_v4/test/simple_input.rb
+++ handlers/remedy_generic_update_v4/test/simple_input.rb
@@ -1,9 +1,9 @@
 {
   'info' => {
-    'server' => 'emu.kineticdata.com',
-    'username' => 'Allen',
-    'password' => 'Allen',
-    'port' => '3000',
+    'server' => '',
+    'username' => '',
+    'password' => '',
+    'port' => '',
     'prognum' => '',
     'authentication' => '',
     'enable_debug_logging' => 'Yes',
@@ -11,8 +11,8 @@
   },
   'parameters' => {
     'error_handling' => 'Error Message',
-    'form' => 'HPD:Help Desk',
-    'request_id' => 'INC000000007504',
-    'field_values' => '{"Detailed Decription":"Updated notes!"}'
+    'form' => '',
+    'request_id' => '',
+    'field_values' => ''
   }
 }
