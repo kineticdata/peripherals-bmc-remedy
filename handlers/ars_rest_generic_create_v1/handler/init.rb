efc95ddd165cac1eb0044af09115e81c41a77eb1
Brian Peterson <brian.peterson@kineticdata.com>
2024-01-26 14:02:55 -0600

    Updates to auth token and making query retrieve consistent with other handlers.

--- handlers/ars_rest_generic_create_v1/handler/init.rb
+++ handlers/ars_rest_generic_create_v1/handler/init.rb
@@ -84,7 +84,7 @@ class ArsRestGenericCreateV1
       end
     else
       # format the headers with the token
-      headers = {:content_type => 'application/json', :authorization => "AR-JWT "+token}
+      headers = {:content_type => 'application/json', :authorization => "AR-JWT "+token, :accept => @accept}
       puts(format_hash("Headers: ", headers)) if @debug_logging_enabled
       
       if error_message==""
@@ -168,12 +168,11 @@ class ArsRestGenericCreateV1
         payload: params,
         headers: { :content_type => :'application/x-www-form-urlencoded' }
       }).execute do |response, request, result|
-        results = response.code
         #if sucessful code will be 200 and the body will be the token for further calls
         #if there is an error response.body will contain the errror in HTML
         if response.code == 200
           puts(result.body) if @debug_logging_enabled
-          return result.body
+          return response.body
         else
           return response.code.to_s
         end
